9cf1acb1d99bec1e8425c98ec9ae1002706ed51f
Martin Urias <nitram92bcs@gmail.com>
2019-11-27 15:52:55 -0700

    Actualizando Proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blue,
-          appBar: AppBar(
-            title: Text('Rancho El Platanito'),
-            backgroundColor: Colors.amber,
-          ),
-          body: Center(
-            child: Image(
-                image: AssetImage('images/big.png'),
+void main() => runApp(RaepApp());
+
+class RaepApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/logo.png'),
               ),
+              Text(
+                'Rancho El Platanito',
+                style: TextStyle(
+                  fontFamily: 'Lobster',
+                  fontSize: 30.0,
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          )
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue,
+          child: Icon(
+              Icons.apps
           ),
-          ),
+        ),
       ),
     );
+  }
+}
